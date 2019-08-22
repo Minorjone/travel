1b67dfb570c4a392e0a766b29ca1eb790a4319b0
dango <879275999@qq.com>
2019-08-22 21:22:55 +0800

    city-router

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Hello from '@/pages/home/Home'
+import City from '@/pages/city/City'
 
 Vue.use(Router)
 
@@ -10,6 +11,10 @@ export default new Router({
       path: '/',
       name: 'Hello',
       component: Hello
+    }, {
+      path: '/city',
+      name: 'City',
+      component: City
     }
   ]
 })
