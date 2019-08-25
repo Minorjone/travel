83e478392de322ff3d5577e99880a4eb84052a96
dango <879275999@qq.com>
2019-08-25 17:41:30 +0800

    performance for scroll city list

--- src/pages/city/components/List.vue
+++ src/pages/city/components/List.vue
@@ -17,8 +17,8 @@
           </div>
         </div>
       </div>
-      <div class="area" v-for="(item,key) of cities" :key="key">
-        <div class="title border-topbottom">A</div>
+      <div class="area" v-for="(item,key) of cities" :key="key" :ref="key">
+        <div class="title border-topbottom">{{key}}</div>
         <div class="item-list">
           <div class="item border-bottom" v-for="innerItem of item" :key="innerItem.id">{{innerItem.name}}</div>
         </div>
@@ -36,7 +36,16 @@ export default {
   },
   props: {
     hot: Array,
-    cities: Object
+    cities: Object,
+    letter: String
+  },
+  watch: {
+    letter () {
+      if (this.letter) {
+        const element = this.$refs[this.letter][0]
+        this.scroll.scrollToElement(element)
+      }
+    }
   }
 }
 </script>
